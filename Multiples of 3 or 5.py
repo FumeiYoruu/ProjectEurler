599ca284d73be47f71813e06e079471bc62a74c8
TokitouYoruu <95714792+TokitouYoruu@users.noreply.github.com>
2021-12-12 00:52:23 -0800

    Update Multiples of 3 or 5.py

--- Multiples of 3 or 5.py
+++ Multiples of 3 or 5.py
@@ -3,6 +3,9 @@ cnt_3=int(999/3)
 cnt_5=int(999/5)
 cnt_15=int(999/15)
 
+#count all the multiples of 3,5 and 15
+#the answer would be the sum of multiples of 3 and 5 subtracted by the sum of multiples of 15
+
 def add(x):
     return int((1+x)*x/2)
 print(3*add(cnt_3)+5*add(cnt_5)-15*add(cnt_15))
