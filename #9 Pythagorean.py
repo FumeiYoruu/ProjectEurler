6b2d26e300813bc66d7167827e80532527f47dfb
TokitouYoruu <95714792+TokitouYoruu@users.noreply.github.com>
2021-12-15 19:51:40 -0800

    Create #9 Pythagorean.py

---
+++ #9 Pythagorean.py
@@ -0,0 +1,9 @@
+#the greates c is 499 for a+b>c
+for c1 in range(499):
+    c=c1+1
+    #for every c, assume a is smaller than b, go over all the a
+    for a1 in range(int((1000-c)/2)):
+        a=a1+1
+        b=1000-c-a
+        if (c**2==a**2+b**2):
+            print(a*b*c)
